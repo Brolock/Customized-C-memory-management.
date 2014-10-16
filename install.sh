c0afc1788af3c0eefa8ea2abf030c25efbac27ba
Marcelin Dupraz <marcelin.dupraz@gmail.com>
2014-10-16 18:33:25 +0200

    install.sh script updated.
        Now compile all the memlib.a with all the log / non-log options.
        Suffixes are added at the end of the mem_lib
        _l with log
        _lt log with time
        _d debug
        _rdi release with debug infos
        _msi minimal size
        One last thing to do is to create a make install command with
        CMake

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
-!#/bin/sh
+#!/bin/sh
 
-mkdir build;
-cd build;
-cmake -DCOMPILE_WITH_LOG=1 -DLOG_WITH_TIME=1 -DCMAKE_BUILD_TYPE=RELEASE \
-            -G"Unix Makefiles" ../;
-#cmake -DCMAKE_BUILD_TYPE=DEBUG -G"Unix Makefiles" ../;
-make
+mkdir build; cd build
+for i in "-DCOMPILE_WITH_LOG=1" "-DCOMPILE_WITH_LOG=1 -DLOG_WITH_TIME=1"
+do
+    for j in "-DCMAKE_BUILD_TYPE=RELEASE" "-DCMAKE_BUILD_TYPE=DEBUG" \
+	"-DCMAKE_BUILD_TYPE=RELWITHDEBINFO" "-DCMAKE_BUILD_TYPE=MINSIZEREL"
+    do
+	    cmake $i $j -G"Unix Makefiles" ..
+	    make
+    done
+done
+
 mkdir ../tests/build
 cd ../tests/build/
 cmake -DCOMPILE_WITH_LOG=1 -DLOG_WITH_TIME=1 -DCMAKE_BUILD_TYPE=RELEASE \
             -G"Unix Makefiles" ../;
-#cmake -DCMAKE_BUILD_TYPE=DEBUG -G"Unix Makefiles" ../;
 make;
-cd ../..;
-rm -rf build tests/build
+cd ../..; rm -rf build tests/build
