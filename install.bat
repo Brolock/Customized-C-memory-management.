b6f377ea29dc817d0e0797803eaa5121d6cc3c9f
Marcelin <mdupraz@novaquark.com>
2014-10-21 12:26:18 +0200

    install.bat is fonctional.
    	Now need to do cmake for the tests on Windows.
    	Also, need to update the make install option so it take a
    	cmake variable and not an absolute path (which could change)

--- install.bat
+++ install.bat
@@ -1,25 +1,30 @@
 pushd %~dp0
 
-::call "%VS120 ...."
-set CMAKE_BIN_PATH=%programfiles%\CMake\bin
+call "%VS120COMNTOOLS%VsDevCmd.bat"
+
+set CMAKE_BIN_PATH=%programfiles(x86)%\CMake\bin
 call cmake_location.bat
 set PATH=%PATH%; %CMAKE_BIN_PATH%
 
 mkdir build
-mkdir build/bat_build_lib
-cd build/bat_build_lib
+cd build
+mkdir bat_build_lib
+cd bat_build_lib
 
-::echo. Version%%~nxa.txt
-for /f "tokens=*" %%J in (../../.memlib_compile_flags.txt) do
-(
-    for /f "tokens=*" %%I in (../../.memlib_build_flags.txt) do
-    (
-        cmake %%I %%J -G"Visual Studio 12 2013 Win64" ../..
-    )
+for /f "tokens=*" %%I in (../../.memlib_compile_flags.txt) do (
+        cmake %%I -G"Visual Studio 12 2013 Win64" ../..
+	msbuild "nq_memlib.sln" /p:Configuration=Release /p:Patform=x64 /m
+	msbuild "nq_memlib.sln" /p:Configuration=RelWithDebInfo /p:Patform=x64 /m
+	msbuild "nq_memlib.sln" /p:Configuration=Debug /p:Patform=x64 /m
+	msbuild "nq_memlib.sln" /p:Configuration=MinSizeRel /p:Patform=x64 /m
+	
+	cd ..
+	rmdir /S /Q bat_build_lib
+	mkdir bat_build_lib
+	cd bat_build_lib
 )
 
-
-msbuild "nq_memlib" /p:Configuration=Release /p:Patform=x64 /m
+cmake -G"Visual Studio 12 2013 Win64" ../..
 
 cd ../..
 
