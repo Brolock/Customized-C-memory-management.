53da0119f256d022f07db1d542a755bde2d5a440
Marcelin <mdupraz@novaquark.com>
2014-10-29 19:59:03 +0100

    printer updated
    	print_file takes a file name instead of an ostream.
    	The print_helper also print the current thread that called
    	the print.
    	still dump to do.

--- domains/nq_memlib/domains.h
+++ domains/nq_memlib/domains.h
@@ -2,12 +2,12 @@
 # define DOMAINS_H_
 
 # include <mutex>
+# include <fstream>
 
 # include <nq_memlib/base_domain.h>
 # include <nq_memlib/lib_domains.h>
 
 
-/* !!! Add your domain to the print method if you want it printed !!!*/
 /* User defined domains */
 # define NQ_USR_DOMAIN(domain_name) NQ_DOMAIN(domain_name)
 # include "domains_decl.h"
@@ -17,18 +17,12 @@ namespace nq { namespace log {
     /* forward declaration of the function defined in nq_log_printer.cpp*/
     void print_helper(std::ostream& os, const char *message);
 
-    /*
-    ** /!\                                 /!\
-    ** After declaring a Domain a user should add his
-    ** MyDomain::getInstance().print() line if he wants it to be printed.
-    ** /!\                                 /!\
-    */
     inline void print(::std::ostream& os = std::cout,
             const char *message = "No specific message")
     {
+# ifdef WITH_NQ_MEMLOG
         static std::mutex mutex;
         std::lock_guard<std::mutex> locker(mutex);
-# ifdef WITH_NQ_MEMLOG
         os << "==================\n";
 
         print_helper(os, message);
@@ -44,6 +38,13 @@ namespace nq { namespace log {
 
         os << "==================\n";
 # endif // !WITH_NQ_MEMLOG
+    }
+
+    inline void print_file(const char *path,
+           const char *message = "No specific message")
+    {
+        std::ofstream file(path, std::ios_base::app);
+        print(file, message);
     }
 }} // namespace nq::log
 
--- source/nq_log_printer.cpp
+++ source/nq_log_printer.cpp
@@ -1,5 +1,13 @@
-# include <iostream>
-# include <ctime>
+#include <iostream>
+#include <ctime>
+
+#include "../include/nq_memlib/env_maccro.h"
+
+#ifdef NQ_GNU_
+# include <pthread.h>
+#else
+# include <Windows.h>
+#endif
 
 namespace nq { namespace log {
     void print_time(std::ostream& os);
@@ -11,18 +19,23 @@ namespace nq { namespace log {
     */
     void print_helper(std::ostream& os, const char *message)
     {
+        os << message << std::endl;
+
+#ifdef NQ_GNU_
+        os << "Thread: " << pthread_self() << std::endl;
+#else // NQ_GNU_ (NQ_WIN_ defined)
+        os << "Thread: " << GetCurrentThreadId() << std::endl;
+#endif // !NQ_GNU_
 #ifdef WITH_NQ_LOGTIME
         print_time(os);
 #endif // !WITH_NQ_LOGTIME
-
-        os << message << std::endl;
     }
 
     /* Print the time of the print in h/min/sec format */
     /* This function is doing 10 allocs */
     void print_time(::std::ostream& os)
     {
-        os << "logged at:";
+        os << "logged at: ";
 
         time_t t = time(0); // get current time
         tm *now = localtime(&t);
